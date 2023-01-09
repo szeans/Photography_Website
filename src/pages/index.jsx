f788937a89479aaf85743d18422ecf3dcb5818a1
Sean Choi <szeanchoi@gmail.com>
2023-01-09 05:20:07 +0200

    mobile

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,12 +5,15 @@ export function Index() {
   return (
     <div className="nav">
 
-        {/* Navbar */}
+      {/* Navbar */}
 
-        <span onselectstart="return false" id="name">
-          <Link to='/' className="name">szean choi</Link>
-        </span>
-        <br /><br />
+      <span onselectstart="return false" id="name">
+        <Link to='/' className="name">szean choi</Link>
+      </span>
+
+      <br /><br />
+      
+      <span id="desktop_nav">
         <span id="pics">
           <NavLink to='/bnw' className="links">b&w</NavLink>
           <NavLink to='/color' className="links">color</NavLink>
@@ -20,7 +23,9 @@ export function Index() {
           <NavLink to='/contact' className="linkss">contact</NavLink>
           <a href="https://mega.nz/folder/rt0k0ZzC#10vGO_SSVE_y0JWA4Ygxwg" target="_blank" className="linkss">archive</a>
         </span>
-      </div>
+      </span>
+
+    </div>
   );
 
 }
